c2b8ef51b3f8cb02b5ca48797a69cfac59f975b1
Bradley Anderson <Brad.Anderson1995@gmail.com>
2015-06-22 15:46:25 -0500

    Beefs up install script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,24 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 platform=$(uname)
-files="bashrc vimrc vim config zshrc oh-my-zsh private scrotwm.conf Xresources cinnamon gimp-2.8 mednafen PyCharm40 themes gitconfig xmodmap pam_environment"
+files="
+bashrc 
+vimrc 
+vim 
+config 
+zshrc 
+oh-my-zsh 
+private 
+scrotwm.conf 
+Xresources 
+cinnamon 
+gimp-2.8 
+mednafen 
+PyCharm40 
+themes 
+gitconfig 
+xmodmap 
+pam_environment"
 # list of files/folders to symlink in homedir
 
 ##########
@@ -32,16 +49,7 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-if [[ $platform == 'Linux' ]]; then
-    echo -n  " Would you like to install powerline fonts? (y/n) "
-    read response
-    if [ $response == y || $response == Y ]; then
-        git clone http://github.com/powerline/fonts.git
-        fonts/install.sh
-        rm -r -f fonts
-    fi
-fi
-
+# Installation functions
 install_tools () {
     yes | sudo apt-get install terminator
     yes | sudo apt-get install tree
@@ -52,58 +60,97 @@ install_tools () {
 }
 
 install_rust_src () {
-if [[ $platform == 'Linux' ]]; then
-    if [[ ! -d /usr/local/src/rust/ ]]; then 
-        sudo git clone https://github.com/rust-lang/rust.git /usr/local/src/rust
-    else
-        echo "Rust source is installed"
+    if [[ $platform == 'Linux' ]]; then
+        if [[ ! -d /usr/local/src/rust/ ]]; then 
+            sudo git clone https://github.com/rust-lang/rust.git /usr/local/src/rust
+        else
+            echo "Rust source is installed"
+        fi
     fi
-fi
 }
 
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
+    # Test to see if zshell is installed.  If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+            git clone http://github.com/robbyrussell/oh-my-zsh.git
+        fi
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
+        fi
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            yes | sudo apt-get install zsh
+            install_zsh
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
     fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        yes | sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
 }
 
 install_terminator () {
-# Test to see if terminator is installed. If it is:
-if [ -f /usr/bin/terminator ]; then
-    # Set default terminal emulator
-    echo "Select your prefered terminal emulator"
-    sudo update-alternatives --config x-terminal-emulator
-else
-    echo "Please install terminator, then re-run this script!"
-    exit
-fi
+    # Test to see if terminator is installed. If it is:
+    if [ -f /usr/bin/terminator ]; then
+        # Set default terminal emulator
+        echo "Select your prefered terminal emulator"
+        sudo update-alternatives --config x-terminal-emulator
+    else
+        echo "Please install terminator, then re-run this script!"
+        exit
+    fi
 }
 
-echo -n "Would you like to install tools? (y/n) "
-read response
-if [ $response == y ] || [ $response == Y ]; then
-    install_tools
-    install_zsh
-    install_rust_src
-    install_terminator
-fi
+prompt_installations() {
+    # Guided install of all necessary programs and assets
+    if [[ $platform == 'Linux' ]]; then
+        echo -n  " Would you like to install powerline fonts? (y/n) "
+        read response
+        if [ $response == y ] || [ $response == Y ]; then
+            git clone http://github.com/powerline/fonts.git
+            fonts/install.sh
+            rm -r -f fonts
+        fi
+
+        echo -n "Default install? (y/n) "
+        read response
+        if [ $response == y ] || [ $response == Y ]; then
+            install_tools
+            install_zsh
+            install_rust_src
+            install_terminator
+        else
+            echo -n "Would you like to install tools? (y/n) "
+            read response
+            if [ $response == y ] || [ $response == Y ]; then
+                install_tools
+            fi
+            
+            echo -n "Would you like to install zsh? (y/n) "
+            read response
+            if [ $response == y ] || [ $response == Y ]; then
+                install_zsh
+            fi
+            
+            echo -n "Would you like to install rust src? (y/n) "
+            read response
+            if [ $response == y ] || [ $response == Y ]; then
+                install_rust_src
+            fi
+            
+            echo -n "Would you like to install terminator? (y/n) "
+            read response
+            if [ $response == y ] || [ $response == Y ]; then
+                install_terminator
+            fi
+        fi
+    fi
+}
+
+prompt_installations
